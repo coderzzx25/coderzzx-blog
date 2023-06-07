6f84bea2a5165b6249c2ca0268e615de722bd35e
coderzzx <2549518530@qq.com>
2023-06-07 13:33:56 +0800

    Add -> home -> 封装内容组件

---
+++ src/global/test.data.ts
@@ -0,0 +1,20 @@
+const data = [
+  {
+    id: 1,
+    title: 'AI Claude使用教程，据说是对标ChatGpt的11111111111111111111',
+    time: '2021-01-01',
+    img: 'https://img-blog.csdnimg.cn/20210101153059990.png',
+    content:
+      '今天这篇博文就教大家如何直接跳过申请门栏，直接使用 Claude AI ！首先我们要注册一个叫做 slack网站，（slack是一款企业内部通信Collaboration工具,它可以团队内部的频繁交流与协作。com/intl/zh-cn/ 注册地址)注册并登陆成功后，我们在slack上创立一个工作区就可以了。接着我们进入slack这个应用网站，[https://w1681953053-qja238503.tab=more_info) 这个就是Claude AI 在slack上的应用位置，点击《在Slack中打开》 这个选项就可以了11111111111111111111111111111。'
+  },
+  {
+    id: 2,
+    title: 'AI Claude使用教程，据说是对标ChatGpt',
+    time: '2021-01-01',
+    img: 'https://img-blog.csdnimg.cn/20210101153059990.png',
+    content:
+      '今天这篇博文就教大家如何直接跳过申请门栏，直接使用 Claude AI ！首先我们要注册一个叫做 slack网站，（slack是一款企业内部通信Collaboration工具,它可以团队内部的频繁交流与协作。com/intl/zh-cn/ 注册地址)注册并登陆成功后，我们在slack上创立一个工作区就可以了。接着我们进入slack这个应用网站，[https://w1681953053-qja238503.tab=more_info) 这个就是Claude AI 在slack上的应用位置，点击《在Slack中打开》 这个选项就可以了。'
+  }
+]
+
+export default data
